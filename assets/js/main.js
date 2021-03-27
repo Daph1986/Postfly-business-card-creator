97ba50f77449065b216a153f02d859ce2ce8bec7
Daph1986 <daphne.heimgartner@gmail.com>
2021-03-27 23:09:37 +0100

    Main.js set up functions for phone modal

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// -- -- -- -- -- -- - modals-- -- -- -- -- -- -
+
+/*Documentation from materialize was not sufficiant to
+let model work, with explanation of my husband(Django Heimgartner)
+I was able to produce this code*/
+
+// INITIALISE MODALS
+(function($) {
+    $.fn.leanModal = function(options) {
+        if ($('.modal').length > 0) {
+            $('.modal').modal(options);
+        }
+    };
+
+    $.fn.openModal = function(options) {
+        $(this).modal(options);
+        $(this).modal('open');
+    };
+
+    $.fn.closeModal = function() {
+        $(this).modal('close');
+    };
+})(jQuery);
+
+$(document).ready(function() {
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+})
