e0986359c231e0b8916f6d7465e22818c061832b
Daph1986 <daphne.heimgartner@gmail.com>
2021-04-02 21:29:38 +0200

    Main.js: beautify code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,56 @@
-// -- -- -- -- -- -- - modals-- -- -- -- -- -- -
+// -------------  Modals ------------- 
 
-/*Documentation from materialize was not sufficiant to
+/* 
+Documentation from materialize was not sufficiant to
 let model work, with explanation of my husband(Django Heimgartner)
-I was able to produce this code*/
+I was able to produce this code 
+*/
 
-// INITIALISE MODALS
+// Initialise Modal
 (function ($) {
-    $.fn.leanModal = function (options) {
-        if ($('.modal').length > 0) {
-            $('.modal').modal(options);
-        }
-    };
+  $.fn.leanModal = function (options) {
+    if ($('.modal').length > 0) {
+      $('.modal').modal(options);
+    }
+  };
 
-    $.fn.openModal = function (options) {
-        $(this).modal(options);
-        $(this).modal('open');
-    };
+  $.fn.openModal = function (options) {
+    $(this).modal(options);
+    $(this).modal('open');
+  };
 
-    $.fn.closeModal = function () {
-        $(this).modal('close');
-    };
+  $.fn.closeModal = function () {
+    $(this).modal('close');
+  };
 })(jQuery);
 
 $(document).ready(function () {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
+  // The "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
 })
 
-// -- -- -- -- -- -- - sample kit request form -- -- -- -- -- -- -
+// ------------- Sample kit request form ------------- 
 
 function sendRequestMail(requestForm) {
-    emailjs.send("service_re41zx8", "sample kit request", {
-        "from_fname": requestForm.fname.value,
-        "from_lname": requestForm.lname.value,
-        "from_address01": requestForm.address01.value,
-        "from_address02": requestForm.address02.value,
-        "from_address03": requestForm.address03.value,
-        "from_address04": requestForm.address04.value,
-        "from_email": requestForm.email.value,
-    })
+  // This ensures that all listed details are send through emailjs
+  emailjs.send("service_re41zx8", "sample kit request", {
+    "from_fname": requestForm.fname.value,
+    "from_lname": requestForm.lname.value,
+    "from_address01": requestForm.address01.value,
+    "from_address02": requestForm.address02.value,
+    "from_address03": requestForm.address03.value,
+    "from_address04": requestForm.address04.value,
+    "from_email": requestForm.email.value,
+  })
     .then(
-        function(response) {
-            alert("Thank you, your request has been sent successfully, you will return to the homepage!!", response);
-            location.replace("index.html");
-        },
-        function(error) {
-            alert("FAILED", error);
-        }
+      function (response) {
+        alert("Thank you, your request has been sent successfully, you will return to the homepage!!", response);
+        location.replace("index.html");
+      },
+      function (error) {
+        alert("FAILED", error);
+      }
     );
-    return false;  // To replace location instead of reload the page
-  }
+  // Ensures that the location.replace is done instead of a reload of the page
+  return false;
+}
