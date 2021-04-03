adc9e417508933dcd609959fb503e90961aabe58
Daph1986 <daphne.heimgartner@gmail.com>
2021-04-03 16:51:29 +0200

    Fabric.js: initial commit, initialise collapsible for materialize

---
+++ assets/js/fabric.js
@@ -0,0 +1,6 @@
+// -------------  Collapsibles ------------- 
+
+// From materialize documentation to initialise the collapsible with jQuery
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
