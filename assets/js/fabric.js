203e58d877dab30dc5953af8c74a76ca84093216
Daph1986 <daphne.heimgartner@gmail.com>
2021-04-03 22:57:44 +0200

    Fabric.js: made a start with the new EmailJS function for the quotation request

--- assets/js/fabric.js
+++ assets/js/fabric.js
@@ -4,3 +4,35 @@
 $(document).ready(function(){
   $('.collapsible').collapsible();
 });
+
+// -------------  Creator form ------------- 
+
+// Validation for EmailJS
+document.onload = function () {
+  emailjs.init("user_2XFUnsxHC1xsdX5DI6gOQ");
+}();
+
+// Need to sort this function out so that all the items are send in EmailJS as well as the preview of the card
+function sendQuotationMail(quotationForm) {
+  // This ensures that all listed details are send through emailjs
+  emailjs.send("service_gcpzmbl", "quotation request", {
+    "from_fname": quotationForm.fname.value,
+    "from_lname": quotationForm.lname.value,
+    "from_email": quotationForm.email.value,
+    "from_size": quotationForm.size.value,
+    "from_method": quotationForm.method.value,
+    "from_paper": quotationForm.paper.value,
+    "from_quantity": quotationForm.quanity.value,
+  })
+    .then(
+      function (response) {
+        alert("Thank you, your qoutation request has been sent successfully, you will return to the homepage!!", response);
+        location.replace("index.html");
+      },
+      function (error) {
+        alert("FAILED", error);
+      }
+    );
+  // Ensures that the location.replace is done instead of a reload of the page
+  return false;
+}
