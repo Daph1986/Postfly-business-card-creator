de7bec2d6c25204d688f912a5e49e90192d165fe
Daph1986 <daphne.heimgartner@gmail.com>
2021-04-25 15:22:29 +0200

    Main.js: beautify code and added jQuery for tooltips

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// -------------  Modals ------------- 
+// ---------------- Modals ---------------- 
 
 // From materialize documentation to initialise the modal on the homepage with jQuery
 $(document).ready(function () {
@@ -11,4 +11,12 @@ $(document).ready(function () {
 // From materialize documentation to initialise the collapsible on the creator page with jQuery
 $(document).ready(function () {
   $('.collapsible').collapsible();
+});
+
+
+// --------------- Tooltips --------------- 
+
+// From materialize documentation to initialise the tooltips on the form page and the creator page with jQuery
+$(document).ready(function () {
+  $('.tooltipped').tooltip();
 });
