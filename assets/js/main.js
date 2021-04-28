61d5a5bc5e4536621caef03fc01823cdad47811a
Daph1986 <daphne.heimgartner@gmail.com>
2021-04-28 14:44:33 +0200

    Main.js: final correction, removed 2 unnecessary empty lines

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,12 @@ $(document).ready(function () {
   $('.modal').modal();
 });
 
-
 // ------------- Collapsibles ------------- 
 
 // From materialize documentation to initialise the collapsible on the creator page with jQuery
 $(document).ready(function () {
   $('.collapsible').collapsible();
 });
-
 
 // --------------- Tooltips --------------- 
 
